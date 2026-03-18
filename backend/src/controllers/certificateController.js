f228691ed8304368e6f7de13947f6c627e3315c4
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-03-19 00:45:55 +0545

    Made a createCertificate logic in certificateController.js

--- backend/src/controllers/certificateController.js
+++ backend/src/controllers/certificateController.js
@@ -1,5 +1,24 @@
+import Certificate from "../models/certificate.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiSuccess.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 export const createCertificate=asyncHandler(async(req,res)=>{
+
+    const {studentId,courseId,title,descriptions,founderSign}=req.body;
+
+    if(!studentId || !courseId ||!title ||!descriptions ||!founderSign ){
+        throw new ApiError(401,"All fields are mandatory");
+    }
+
+    const certificate=await Certificate.create({
+        student:studentId,
+        courses:courseId,
+        title:title,
+        descriptions:descriptions,
+        founderSign:founderSign
+    })
+
+return res.status(200).json(new ApiResponse(200,"Certificate created successfully",certificate));
 
 })
