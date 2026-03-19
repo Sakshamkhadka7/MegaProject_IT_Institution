02a7620a184f31d999e4e41c295b4f9d688b3f39
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-03-19 23:26:07 +0545

    Added a getMyCertificate logic in a certificateController.js

--- backend/src/controllers/certificateController.js
+++ backend/src/controllers/certificateController.js
@@ -3,22 +3,40 @@ import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiSuccess.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
-export const createCertificate=asyncHandler(async(req,res)=>{
+export const createCertificate = asyncHandler(async (req, res) => {
+  const { studentId, courseId, title, descriptions, founderSign } = req.body;
 
-    const {studentId,courseId,title,descriptions,founderSign}=req.body;
+  if (!studentId || !courseId || !title || !descriptions || !founderSign) {
+    throw new ApiError(401, "All fields are mandatory");
+  }
 
-    if(!studentId || !courseId ||!title ||!descriptions ||!founderSign ){
-        throw new ApiError(401,"All fields are mandatory");
-    }
+  const certificate = await Certificate.create({
+    student: studentId,
+    courses: courseId,
+    title: title,
+    descriptions: descriptions,
+    founderSign: founderSign,
+  });
 
-    const certificate=await Certificate.create({
-        student:studentId,
-        courses:courseId,
-        title:title,
-        descriptions:descriptions,
-        founderSign:founderSign
-    })
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Certificate created successfully", certificate),
+    );
+});
 
-return res.status(200).json(new ApiResponse(200,"Certificate created successfully",certificate));
+export const getMyCertificate=asyncHandler(async(req,res)=>{
+
+  const studentId=req.user._id;
+  if(!studentId){
+    throw new ApiError(401,"Id couldnot found");
+  }
+  const certifcate=await Certificate.findById({_id:studentId}).populate("courses","title");
+  if(!certifcate){
+    throw new ApiError(401,"Couldnot found certificate");
+  }
+  return res.status(200).json(new ApiResponse(200,"Certificate fetched successfully",certifcate));
 
 })
+
+
