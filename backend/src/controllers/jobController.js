8238383ae58c96ede3545d2bfbfad360c8385345
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-03-24 23:53:35 +0545

    created a createJob controller logic in a jobController.js

---
+++ backend/src/controllers/jobController.js
@@ -0,0 +1,36 @@
+import Job from "../models/job.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiSuccess.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+export const createJob = asyncHandler(async (req, res) => {
+  const adminId = req.user?._id;
+  if (req.user.role !== "Admin") {
+    throw new ApiError(401, "Not authorized to upload a job");
+  }
+  const { title, company, location, position, description } = req.body;
+  if (!title || !company || !location || !position || !description) {
+    throw new ApiError(401, "All fields are mandatory");
+  }
+
+  const jobExists = await Job.find({
+    title: title,
+    company: company,
+    position: position,
+  });
+
+  if (jobExists) {
+    throw new ApiError(401, "Job already exists");
+  }
+
+  const job = await Job.create({
+    title,
+    company,
+    location,
+    position,
+    description,
+    postedBy: adminId,
+  });
+
+  return res.status(200).json(new ApiResponse(200, "Job created successfully"));
+});
--- backend/src/models/job.js
+++ backend/src/models/job.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
 
-const jobSchema=new mongoose.Schema({
-    title:{
-    type:String,
-    required:true
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    company:{
-          type:String,
-    required:true
+    company: {
+      type: String,
+      required: true,
     },
-    location:{
-          type:String,
+    location: {
+      type: String,
     },
-    position:{
-          type:String,
-    required:true
+    position: {
+      type: String,
+      required: true,
     },
-    description:{
-          type:String,
-    required:true
+    description: {
+      type: String,
+      required: true,
     },
-    postedBy:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Student"
-    }
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-},{
-    timestamps:true
-});
-
-const Job=mongoose.model("Job",jobSchema);
+const Job = mongoose.model("Job", jobSchema);
 export default Job;
-
